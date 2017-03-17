af1bb8fb7ab2863d8c3677397609d764112da7cf
Ian Caffey <iancaffey@ymail.com>
2017-03-17 11:12:15 -0500

    Moved route decorator out of unit tests class.

--- app/tests.py
+++ app/tests.py
@@ -23,43 +23,44 @@ class EklogiTests(TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def route(route, *, method='get', code='200 OK'):  # pylint: disable=no-self-argument
-        """
-        Performs a basic check against a route.
-
-        :param route: the route path to query
-        :param method: the name of the method to perform on the client (get, post, etc.)
-        :param code: the response code to expect from the route
-        :return: the route check wrapper
-        """
-
-        def wrap(f):  # pylint: disable=invalid-name
-            """
-            Returns a decorator which performs all response checks.
-
-            :param f: the function to perform additional response checks
-            :return: the check function to query client and test the response
-            """
-
-            def check(self):
-                """
-                Queries the client at the specified route, checking the status and data.
-
-                """
-                res = getattr(self.app, method)(route)
-                self.assertEqual(code, res.status)
-                f(self, res)
-
-            return check
-
-        return wrap
-
     @route('/')
     def test_index(self, res):
         """
         Checks the route to '/' exists and returns the test string 'Hello World'.
         """
         self.assertEqual(b'Hello World!', res.data)
+
+
+def route(path, *, method='get', code='200 OK'):  # pylint: disable=no-self-argument
+    """
+    Performs a basic check against a route.
+
+    :param path: the route path to query
+    :param method: the name of the method to perform on the client (get, post, etc.)
+    :param code: the response code to expect from the route
+    :return: the route check wrapper
+    """
+
+    def wrap(f):  # pylint: disable=invalid-name
+        """
+        Returns a decorator which performs all response checks.
+
+        :param f: the function to perform additional response checks
+        :return: the check function to query client and test the response
+        """
+
+        def check(self):
+            """
+            Queries the client at the specified route, checking the status and data.
+
+            """
+            res = getattr(self.app, method)(path)
+            self.assertEqual(code, res.status)
+            f(self, res)
+
+        return check
+
+    return wrap
 
 
 if __name__ == '__main__':
