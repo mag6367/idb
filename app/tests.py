cfccf1cc06be2479c625330e8578560795c5da05
Ian Caffey <iancaffey@ymail.com>
2017-03-16 19:41:53 -0500

    Added basic route decorator for generating east route unit tests.

--- app/tests.py
+++ app/tests.py
@@ -8,6 +8,7 @@ Route tests ensure attempts to reach a route return a 200 OK response and the da
 Model unit tests ensure database schema validity, testing for loading, saving, and updating entries.
 
 """
+from functools import wraps
 from unittest import main, TestCase
 
 from app.eklogi import app  # pylint: disable=import-error
@@ -23,13 +24,36 @@ class EklogiTests(TestCase):
     def setUp(self):
         self.app = app.test_client()
 
+    @wraps
+    def routeCheck(self, f):
+        """
+        Performs a basic check against a route.
+
+        The route meta data must be an iterable which contains the
+        route path, response code/status, and expected result.
+
+        :param f: the route meta data
+        :param extra: any extra checks to perform on the response
+        :return: the route check wrapper
+        """
+        route, code, expected = f()
+
+        def check():
+            """
+            Queries the client at the specified route, checking the status and data.
+            """
+            res = self.app.get(route)
+            self.assertEqual(code, res.status)
+            self.assertEqual(expected, res.data)
+
+        return check
+
+    @routeCheck
     def test_index(self):
         """
-        Checks the route to '/' exists and returns the test string 'Hello World'
+        Checks the route to '/' exists and returns the test string 'Hello World'.
         """
-        res = self.app.get('/')
-        self.assertEqual('200 OK', res.status)
-        self.assertEqual(b'Hello World!', res.data)
+        return '/', '200 OK', b'Hello World!'
 
 
 if __name__ == '__main__':
