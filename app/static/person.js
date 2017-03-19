018fdae341c9332e80ba2a1730030f1b530a5ccc
Evan Durfee <evanmdurfee@gmail.com>
2017-03-19 12:57:03 -0500

    Reconfigures the person display page to only retrieve jsonn data once, and adds some basic dynamic loading support to the url (?id=## retrieves example_person_##.json). Closes #18.

--- app/static/person.js
+++ app/static/person.js
@@ -1,20 +1,16 @@
 var loadJSONWrapper= function(path, callback) {
   $.ajax({
     url: path,
-    success: function(content) {
-      if(content) {
-        callback(content);
-        // personData.content = content;
-        // alert("Setting to " + personData.content.Name);
-      }
-      else {
-        alert("Error reading person");
-      }
-    },
+    success: callback,
     error: function(result){
         alert("Error reading person");
     }
   });
+}
+
+var getPerson = function() {
+  var searchParams = new URLSearchParams(window.location.search);
+  return searchParams.get("id");
 }
 
 function PersonPortrait(props) {
@@ -49,6 +45,7 @@ function PersonDetails(props) {
         <p id="person-party">Party affiliation: {props.person.PartyAffiliation}</p>
         <PersonLifespan person={props.person}/>
         <a id="person-website" href="#">Website</a>
+        <p>{getPerson()}</p>
       </div>
       {/*
       <div className="caption text-center">
@@ -64,36 +61,14 @@ function PersonDetails(props) {
 class PersonView extends React.Component {
   constructor() {
     super();
-    this.state = {
-      person: null
-    };
-  }
-
-  componentDidMount() {
-    var that = this;
-    loadJSONWrapper("example_person_01.json", function(data) {
-      that.updatePerson(data);
-    });
-  }
-
-  updatePerson(personData) {
-    var that = this;
-    if(personData) {
-      that.setState({
-        person: personData,
-      });
-    }
-    else {
-      alert("ELSE2");
-    }
   }
 
   render() {
-    if(this.state.person) {
+    if(this.props.person) {
       return (
         <div className="thumbnail">
-          <PersonPortrait person={this.state.person}/>
-          <PersonDetails person={this.state.person}/>
+          <PersonPortrait person={this.props.person}/>
+          <PersonDetails person={this.props.person}/>
         </div>
       );
     }
@@ -150,34 +125,14 @@ class OfficeView extends React.Component {
     super();
     this.state = {
       officeIndex: 0,
-      person: null
     };
   }
 
-  componentDidMount() {
-    var that = this;
-    loadJSONWrapper("example_person_01.json", function(data) {
-      that.updatePerson(data);
-    });
-  }
-
-  updatePerson(personData) {
-    var that = this;
-    if(personData) {
-      that.setState({
-        person: personData,
-      });
-    }
-    else {
-      alert("ELSE2");
-    }
-  }
-
   render() {
-    if(!this.state.person) {
+    if(!this.props.person) {
       return (<div id="office-view-content"><p>No Data</p></div>);
     }
-    var office = this.state.person.Offices[this.state.officeIndex];
+    var office = this.props.person.Offices[this.state.officeIndex];
     return (
       <div id="office-view">
         <div className="panel panel-default">
@@ -227,7 +182,7 @@ class Person extends React.Component {
 
   componentDidMount() {
     var that = this;
-    loadJSONWrapper("example_person_01.json", function(data) {
+    loadJSONWrapper("example_person_" + getPerson() + ".json", function(data) {
       that.updatePerson(data);
     });
   }
@@ -238,6 +193,7 @@ class Person extends React.Component {
       that.setState({
         person: personData,
       });
+
     }
     else {
       alert("ELSE2");
@@ -245,7 +201,30 @@ class Person extends React.Component {
   }
 
   render() {
-    return (<p>TBD</p>);
+    return (
+      <div className="container">
+    		<div className="row">
+    			<div className="col-sm-6">
+    				<PersonView person={this.state.person}/>
+    			</div>
+    			<div className="col-sm-6">
+    				<OfficeView person={this.state.person}/>
+    			</div>
+    		</div>
+        {/*
+    		<div className="row">
+    			<div className="col-sm-12">
+    				<div className="panel panel-default">
+    					<div className="panel-content" id="person-summary">
+    						Summary
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+        */}
+    	</div>
+
+    );
   }
 
 }
@@ -255,11 +234,16 @@ class Person extends React.Component {
 // }
 
 ReactDOM.render(
-  <PersonView />,
-  document.getElementById('person-info-view')
+  <Person />,
+  document.getElementById('person')
 );
 
-ReactDOM.render(
-  <OfficeView />,
-  document.getElementById('person-office-view')
-);
+// ReactDOM.render(
+//   <PersonView />,
+//   document.getElementById('person-info-view')
+// );
+//
+// ReactDOM.render(
+//   <OfficeView />,
+//   document.getElementById('person-office-view')
+// );
