395f9189c8c5baa021b63570f800b455dfc05fd6
Nick Dant <n.dant@utexas.edu>
2017-03-23 18:33:53 -0500

    About page complete. Closes #59. Need to fill in bios and contributions

---
+++ app/static/js/about.js
@@ -0,0 +1,315 @@
+class AboutNick extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+
+        return (
+            <div className="thumbnail">
+                <div id="person-details">
+                    <div className="caption text-center">
+                        <img src="../static/img/nick.jpeg" style={{width: 300 + 'px'}}/>
+                        <h2>Nick Dant</h2>
+                        <a href="https://github.com/NickDant">
+                            <img src="../static/img/Github-Mark-32px.png"/>
+                        </a>
+                    </div>
+                </div>
+                <div className="caption text-center">
+                    <h4>Bio</h4>
+                    <p>Nick likes pizza</p>
+                </div>
+                <div className="row">
+
+                    <div className="col-sm-6">
+                        <div className="caption text-center">
+                            <h4>Roles</h4>
+                            <p>Frontend, UML</p>
+                        </div>
+                    </div>
+                    <div className="col-sm-6">
+                        <div className="caption text-center">
+                            <h4>Contributions</h4>
+                            <p>Commits: </p>
+                            <p>Issues: </p>
+                            <p>Unit Tests: </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+class AboutIan extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+
+        return (
+            <div className="thumbnail">
+                <div id="person-details">
+                    <div className="caption text-center">
+                        <img src="../static/img/ian.jpeg" style={{width: 300 + 'px'}}/>
+                        <h2>Ian Caffey</h2>
+                        <a href="https://github.com/iancaffey">
+                            <img src="../static/img/Github-Mark-32px.png"/>
+                        </a>
+
+                    </div>
+                    <div className="caption text-center">
+                        <h4>Bio</h4>
+                        <p>Ian likes pizza</p>
+                    </div>
+                    <div className="row">
+
+                        <div className="col-sm-6">
+                            <div className="caption text-center">
+                                <h4>Roles</h4>
+                                <p>Project Lead Phase 1, Design, Backend</p>
+                            </div>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="caption text-center">
+                                <h4>Contributions</h4>
+                                <p>Commits: </p>
+                                <p>Issues: </p>
+                                <p>Unit Tests: </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+class AboutJared extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+
+        return (
+            <div className="thumbnail">
+                <div id="person-details">
+                    <div className="caption text-center">
+                        <img src="../static/img/jared.jpeg" style={{width: 300 + 'px'}}/>
+                        <h2>Jared McArthur</h2>
+                        <a href="https://github.com/ascensionra">
+                            <img src="../static/img/Github-Mark-32px.png"/>
+                        </a>
+                    </div>
+                    <div className="caption text-center">
+                        <h4>Bio</h4>
+                        <p>Jared likes pizza</p>
+                    </div>
+                    <div className="row">
+
+                        <div className="col-sm-6">
+                            <div className="caption text-center">
+                                <h4>Roles</h4>
+                                <p>Backend, Hosting</p>
+                            </div>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="caption text-center">
+                                <h4>Contributions</h4>
+                                <p>Commits: </p>
+                                <p>Issues: </p>
+                                <p>Unit Tests: </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+class AboutEvan extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+
+        return (
+            <div className="thumbnail">
+                <div id="person-details">
+                    <div className="caption text-center">
+                        <img src="../static/img/evan.jpeg" style={{width: 300 + 'px'}}/>
+                        <h2>Evan Durfee</h2>
+                        <a href="https://github.com/durfeeUT">
+                            <img src="../static/img/Github-Mark-32px.png"/>
+                        </a>
+                    </div>
+                    <div className="caption text-center">
+                        <h4>Bio</h4>
+                        <p>Evan likes pizza</p>
+                    </div>
+                    <div className="row">
+
+                        <div className="col-sm-6">
+                            <div className="caption text-center">
+                                <h4>Roles</h4>
+                                <p>Design, Frontend, UML</p>
+                            </div>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="caption text-center">
+                                <h4>Contributions</h4>
+                                <p>Commits: </p>
+                                <p>Issues: </p>
+                                <p>Unit Tests: </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+class AboutRick extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+
+        return (
+            <div className="thumbnail">
+                <div id="person-details">
+                    <div className="caption text-center">
+                        <img src="../static/img/rick.png" style={{width: 300 + 'px'}}/>
+                        <h2>Rick</h2>
+                        <a href="https://github.com/RickHasAGit">
+                            <img src="../static/img/Github-Mark-32px.png"/>
+                        </a>
+                    </div>
+                    <div className="caption text-center">
+                        <h4>Bio</h4>
+                        <p>Rick likes pizza</p>
+                    </div>
+                    <div className="row">
+
+                        <div className="col-sm-6">
+                            <div className="caption text-center">
+                                <h4>Roles</h4>
+                                <p>Design, Frontend</p>
+                            </div>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="caption text-center">
+                                <h4>Contributions</h4>
+                                <p>Commits: </p>
+                                <p>Issues: </p>
+                                <p>Unit Tests: </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+class AboutMarco extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+
+        return (
+            <div className="thumbnail">
+                <div id="person-details">
+                    <div className="caption text-center">
+                        <img src="../static/img/marco.png" style={{width: 300 + 'px'}}/>
+                        <h2>Marco Guajardo</h2>
+                        <a href="https://github.com/mag6367">
+                            <img src="../static/img/Github-Mark-32px.png"/>
+                        </a>
+                    </div>
+                    <div className="caption text-center">
+                        <h4>Bio</h4>
+                        <p>Marco likes pizza</p>
+                    </div>
+                    <div className="row">
+
+                        <div className="col-sm-6">
+                            <div className="caption text-center">
+                                <h4>Roles</h4>
+                                <p>Design, Documentation</p>
+                            </div>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="caption text-center">
+                                <h4>Contributions</h4>
+                                <p>Commits: </p>
+                                <p>Issues: </p>
+                                <p>Unit Tests: </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+
+class About extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            about: null
+        };
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-4">
+                        <AboutJared />
+                    </div>
+                    <div className="col-sm-4">
+                        <AboutMarco />
+                    </div>
+                    <div className="col-sm-4">
+                        <AboutIan />
+                    </div>
+                    <div className="col-sm-4">
+                        <AboutEvan />
+                    </div>
+                    <div className="col-sm-4">
+                        <AboutRick />
+                    </div>
+                    <div className="col-sm-4">
+                        <AboutNick />
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+
+}
+
+ReactDOM.render(
+    <About />,
+    document.getElementById('about')
+);
+
